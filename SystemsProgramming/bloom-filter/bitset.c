bca6bb49fb2e90add5254b1ef046d69342bb794f
Sean Gaffney <seangaffney@netgear783.ljsoft.net>
2020-11-17 14:30:49 +0000

    Completed both bitsec and bloom

--- SystemsProgramming/bloom-filter/bitset.c
+++ SystemsProgramming/bloom-filter/bitset.c
@@ -2,37 +2,98 @@
 
 // create a new, empty bit vector set with a universe of 'size' items
 struct bitset * bitset_new(int size) {
+    struct bitset * temp;
+	temp = malloc(sizeof(struct bitset));
+	int size_in_words = ((size - 1)/64) + 1;
+	uint64_t * bits = malloc(sizeof(uint64_t*)*size_in_words);
+	for (int i = 0; i < size_in_words; i++) {
+		bits[i] = 0;
+	}
+	temp -> bits = bits;
+	temp -> size_in_words = size_in_words;
+	temp -> universe_size = size_in_words*64;
+	return temp;
 }
 
 // get the size of the universe of items that could be stored in the set
 int bitset_size(struct bitset * this) {
+    int universeSize = this -> universe_size;
+    return universeSize;
 }
 
 // get the number of items that are stored in the set
 int bitset_cardinality(struct bitset * this){
+    int count = 0;
+    for (int i = 0; i < this -> size_in_words; i++) {
+        uint64_t item = this -> bits[i];
+        while(item != 0) {
+            if(item % 2 == 1) {
+                count++;
+            }
+            item = item/2;
+        }
+    }
+    return count;
 }
 
 // check to see if an item is in the set
 int bitset_lookup(struct bitset * this, int item){
+    int block = item / 64;
+    int index = item % 64;
+    uint64_t word = this -> bits[block];
+    uint64_t mask = 1ULL << 63;
+    uint64_t result = word << index;
+    if (result & mask) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 // add an item, with number 'item' to the set
 // has no effect if the item is already in the set
 void bitset_add(struct bitset * this, int item) {
+    int block = item / 64;
+	int index = item % 64;
+	uint64_t word = this -> bits[block];
+	uint64_t mask = 1UL << (63 - index);
+	uint64_t result = word | mask;
+	this -> bits[block] = result;
 }
 
 // remove an item with number 'item' from the set
 void bitset_remove(struct bitset * this, int item) {
+    int block = item / 64;
+	int index = item % 64;
+	uint64_t word = this -> bits[block];
+	uint64_t mask = ~0UL;
+	mask = mask ^ (1UL << (63 -index));
+	this -> bits[block] = word & mask;
 }
 
 // place the union of src1 and src2 into dest;
 // all of src1, src2, and dest must have the same size universe
-void bitset_union(struct bitset * dest, struct bitset * src1,
-    struct bitset * src2) {
+void bitset_union(struct bitset * dest, struct bitset * src1, struct bitset * src2) {
+    int size = bitset_size(src1);
+	for (int i = 0; i < size; i++) {
+		if (bitset_lookup(src1, i) == 1 || bitset_lookup(src2, i) == 1) {
+			bitset_add(dest, i);
+		} else {
+			bitset_remove(dest, i);
+		}
+	}
 }
 
 // place the intersection of src1 and src2 into dest
 // all of src1, src2, and dest must have the same size universe
-void bitset_intersect(struct bitset * dest, struct bitset * src1,
-    struct bitset * src2) {
+void bitset_intersect(struct bitset * dest, struct bitset * src1, struct bitset * src2) {
+    int size = bitset_size(src1);
+	for (int i = 0; i < size; i++) {
+		if (bitset_lookup(src1, i) == 1 && bitset_lookup(src2, i) == 1) {
+			bitset_add(dest, i);
+		} else {
+			bitset_remove(dest, i);
+		}
+	}
 }
--- SystemsProgramming/bloom-filter/bloom.c
+++ SystemsProgramming/bloom-filter/bloom.c
@@ -5,45 +5,70 @@ const int BLOOM_HASH2 = 29;
 
 // compute a hash of a string using a seed value, where the result
 // falls between zero and range-1
-int hash_string(char * string, int seed, int range)
-{
-  int i;
-  int hash = 0;
+int hash_string(char* string, int seed, int range) {
+    int i;
+    int hash = 0;
 
-  // simple loop for mixing the input string
-  for ( i = 0; string[i] != '\0'; i++ ) {
-    hash = hash * seed + string[i];
-  }
-  // check for unlikely case that hash is negative
-  if ( hash < 0 ) {
-    hash = -hash;
-  }
-  // bring the hash within the range 0..range-1
-  hash = hash % range;
+    // simple loop for mixing the input string
+    for (i = 0; string[i] != '\0'; i++) {
+        hash = hash * seed + string[i];
+    }
+    // check for unlikely case that hash is negative
+    if (hash < 0) {
+        hash = -hash;
+    }
+    // bring the hash within the range 0..range-1
+    hash = hash % range;
 
-  return hash;
+    return hash;
 }
 
-
 // create a new, empty Bloom filter of 'size' items
-struct bloom * bloom_new(int size) {
+struct bloom* bloom_new(int size) {
+    struct bloom* temp;
+    temp = malloc(sizeof(struct bloom));
+    struct bitset* newSet = bitset_new(size);
+    temp->size = size;
+    temp->bitset = newSet;
+    return temp;
 }
 
 // check to see if a string is in the set
-int bloom_lookup(struct bloom * this, char * item) {
+int bloom_lookup(struct bloom* this, char* item) {
+    struct bitset* newSet = this->bitset;
+    int first = hash_string(item, BLOOM_HASH1, this->size);
+    int second = hash_string(item, BLOOM_HASH2, this->size);
+    if (bitset_lookup(newSet, first) == 1 ||
+        bitset_lookup(newSet, second) == 1) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 // add a string to the set
 // has no effect if the item is already in the set
-void bloom_add(struct bloom * this, char * item) {
+void bloom_add(struct bloom* this, char* item) {
+    struct bitset* newSet = this->bitset;
+    int first = hash_string(item, BLOOM_HASH1, this->size);
+    int second = hash_string(item, BLOOM_HASH2, this->size);
+    bitset_add(newSet, first);
+    bitset_add(newSet, second);
 }
 
 // place the union of src1 and src2 into dest
-void bloom_union(struct bloom * dest, struct bloom * src1,
-    struct bloom * src2) {
+void bloom_union(struct bloom* dest, struct bloom* src1, struct bloom* src2) {
+    struct bitset* set1 = dest->bitset;
+    struct bitset* set2 = src1->bitset;
+    struct bitset* set3 = src2->bitset;
+    bitset_union(set1, set2, set3);
 }
 
 // place the intersection of src1 and src2 into dest
-void bloom_intersect(struct bloom * dest, struct bloom * src1,
-    struct bloom * src2) {
+void bloom_intersect(struct bloom* dest, struct bloom* src1,
+                     struct bloom* src2) {
+    struct bitset* set1 = dest->bitset;
+    struct bitset* set2 = src1->bitset;
+    struct bitset* set3 = src2->bitset;
+    bitset_intersect(set1, set2, set3);
 }
