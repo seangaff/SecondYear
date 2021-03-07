b4d707dddad5482b0432ba0af0795ec93dcbf739
seangaff <gaffnese@gmail.com>
2021-03-07 14:00:57 -0800

    Pracical2 v1

--- ConcurrentSystems/Practical2/practical2.c
+++ ConcurrentSystems/Practical2/practical2.c
@@ -9,85 +9,104 @@
 #include "cond.c"
 
 
-int pnum;  // number updated when producer runs.
-int csum;  // sum computed using pnum when consumer runs.
+int active = 0;
+int pnum; // number updated when producer runs.
+int csum; // sum computed using pnum when consumer runs.
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 int (*pred)(int); // predicate indicating if pnum is to be consumed
 
 int produceT() {
-  scanf("%d",&pnum); // read a number from stdin
-  return pnum;
+    scanf("%d", &pnum); // read a number from stdin
+    return pnum;
 }
 
 void *Produce(void *a) {
-  int p;
+    int p;
 
-  p=1;
-  while (p) {
-    printf("@P-READY\n");
-    p = produceT();
-    printf("@PRODUCED %d\n",p);
-  }
-  printf("@P-EXIT\n");
-  pthread_exit(NULL);
+    p = 1;
+    while (p) {
+        printf("@P-READY\n");
+        pthread_mutex_lock(&mutex);
+        while (active == 1) {
+            pthread_cond_wait(&cond, &mutex);
+        }
+        p = produceT();
+        active = 1;
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&mutex);
+        printf("@PRODUCED %d\n", p);
+    }
+    printf("@P-EXIT\n");
+    pthread_exit(NULL);
 }
 
-
 int consumeT() {
-  if ( pred(pnum) ) { csum += pnum; }
-  return pnum;
+    if (pred(pnum)) {
+        csum += pnum;
+    }
+    return pnum;
 }
 
 void *Consume(void *a) {
-  int p;
+    int p;
 
-  p=1;
-  while (p) {
-      printf("@C-READY\n");
-    p = consumeT();
-    printf("@CONSUMED %d\n",csum);
-  }
-  printf("@C-EXIT\n");
-  pthread_exit(NULL);
+    p = 1;
+    while (p) {
+        printf("@C-READY\n");
+        pthread_mutex_lock(&mutex);
+        while (active == 0) {
+            pthread_cond_wait(&cond, &mutex);
+        }
+        p = consumeT();
+        active = 0;
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&mutex);
+        printf("@CONSUMED %d\n", csum);
+    }
+    printf("@C-EXIT\n");
+    pthread_exit(NULL);
 }
 
+int main(int argc, const char *argv[]) {
+    // the current number predicate
+    static pthread_t prod, cons;
+    long rc;
 
-int main (int argc, const char * argv[]) {
-  // the current number predicate
-  static pthread_t prod,cons;
-	long rc;
+    pred = &cond1;
+    if (argc > 1) {
+        if (!strncmp(argv[1], "2", 10)) {
+            pred = &cond2;
+        } else if (!strncmp(argv[1], "3", 10)) {
+            pred = &cond3;
+        }
+    }
 
-  pred = &cond1;
-  if (argc>1) {
-    if      (!strncmp(argv[1],"2",10)) { pred = &cond2; }
-    else if (!strncmp(argv[1],"3",10)) { pred = &cond3; }
-  }
+    pnum = 999;
+    csum = 0;
+    srand(time(0));
 
+    printf("@P-CREATE\n");
+    rc = pthread_create(&prod, NULL, Produce, (void *)0);
+    if (rc) {
+        printf("@P-ERROR %ld\n", rc);
+        exit(-1);
+    }
+    printf("@C-CREATE\n");
+    rc = pthread_create(&cons, NULL, Consume, (void *)0);
+    if (rc) {
+        printf("@C-ERROR %ld\n", rc);
+        exit(-1);
+    }
 
-  pnum = 999;
-  csum=0;
-  srand(time(0));
+    printf("@P-JOIN\n");
+    pthread_join(prod, NULL);
+    printf("@C-JOIN\n");
+    pthread_join(cons, NULL);
 
-  printf("@P-CREATE\n");
- 	rc = pthread_create(&prod,NULL,Produce,(void *)0);
-	if (rc) {
-			printf("@P-ERROR %ld\n",rc);
-			exit(-1);
-		}
-  printf("@C-CREATE\n");
- 	rc = pthread_create(&cons,NULL,Consume,(void *)0);
-	if (rc) {
-			printf("@C-ERROR %ld\n",rc);
-			exit(-1);
-		}
+    printf("@CSUM=%d.\n", csum);
 
-  printf("@P-JOIN\n");
-  pthread_join( prod, NULL);
-  printf("@C-JOIN\n");
-  pthread_join( cons, NULL);
-
-
-  printf("@CSUM=%d.\n",csum);
-
-  return 0;
+    return 0;
 }
