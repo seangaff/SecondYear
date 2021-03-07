fee57ea12023cb1edace2070655719bf30a7fd04
seangaff <gaffnese@gmail.com>
2021-03-07 12:36:32 -0800

    Sorting algorithms assignment

--- AlgorithmsAndDataStructuresII/Assignment1/SortComparison.java
+++ AlgorithmsAndDataStructuresII/Assignment1/SortComparison.java
@@ -4,9 +4,10 @@
  *  This class contains static methods that implementing sorting of an array of numbers
  *  using different sort algorithms.
  *
- *  @author
+ *  @author Sean Gaffney
  *  @version HT 2020
  */
+import java.util.Arrays;
 
  class SortComparison {
 
@@ -18,8 +19,16 @@
      *
      */
     static double [] insertionSort (double a[]){
-
-        //todo: implement the sort
+        for(int i = 1; i < a.length; i++) {
+            double key = a[i];
+            int j = i - 1;
+            while(j >= 0 && a[j] > key) {
+                a[j + 1] = a [j];
+                j = j -1;
+            }
+            a[j + 1] = key;
+        }
+        return a;
     }//end insertionsort
 	
 	    /**
@@ -29,10 +38,17 @@
      * @return array sorted in ascending order
      *
      */
-    static double [] selectionSort (double a[]){
-
-         //todo: implement the sort
-
+    static double [] selectionSort (double a[]) {
+        for(int i = 0; i < a.length; i++) {
+            int min = i;
+            for(int j = i + 1; j < a.length; j++) {
+                if(a[j] < a[min]) {
+                    min = j;
+                }
+            }
+            swap(a, i, min);
+        }
+        return a;
     }//end selectionsort
 
     /**
@@ -42,12 +58,33 @@
      * @return array sorted in ascending order
      *
      */
-    static double [] quickSort (double a[]){
-	
-		 //todo: implement the sort
-
+    static double [] quickSort (double a[]) {
+        quickSort(a, 0, a.length - 1);
+        return a;
     }//end quicksort
 
+    //quicksort recurisve
+    private static void quickSort(double a[], int low, int high) {
+        if (low < high) {
+            int pivot = partition(a, low, high);
+            quickSort(a, low, pivot - 1);
+            quickSort(a, pivot + 1, high);
+        }
+    }
+
+    //partition for quicksort
+    private static int partition(double a[], int low, int high) {
+        double pivot = a[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (a[j] <= pivot) {
+                i++;
+                swap(a, i, j);
+            }
+        }
+        swap(a, i + 1, high);
+        return i + 1;
+    }
     /**
      * Sorts an array of doubles using Merge Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
@@ -62,22 +99,51 @@
      * @param a: An unsorted array of doubles.
      * @return after the method returns, the array must be in ascending sorted order.
      */
-
     static double[] mergeSort (double a[]) {
+        int low = 0;
+        int high = a.length - 1;
 
-		 //todo: implement the sort
-	
-    }//end mergesort
-    
+        double[] temp = Arrays.copyOf(a, a.length);
 
+        for (int m = 1; m <= high - low; m = 2 * m) {
+            for (int bottom = low; bottom < high; bottom += 2 * m) {
+                int mid = bottom + m - 1;
+                int top = bottom + 2 * m - 1;
 
+                merge(a, temp, bottom, mid, (top < high) ? top : high);
+            }
+        }
+        return a;
+    }
 
-   
+    //
+    private static void merge(double[] a, double[] temp, int bottom, int mid, int top) {
+        int k = bottom, i = bottom, j = mid + 1;
+        while (i <= mid && j <= top) {
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            } else {
+                temp[k++] = a[j++];
+            }
+        }
+        while (i <= mid && i < a.length) {
+            temp[k++] = a[i++];
+        }
+        for (i = bottom; i <= top; i++) {
+            a[i] = temp[i];
+        }
+    }
 
+    //swaps two values
+    private static void swap(double[] a, int i, int j) {
+        double temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
     public static void main(String[] args) {
 
-        //todo: do experiments as per assignment instructions
+        //expirments done in main of test file
     }
 
  }//end class
--- AlgorithmsAndDataStructuresII/Assignment1/SortComparisonTest.java
+++ AlgorithmsAndDataStructuresII/Assignment1/SortComparisonTest.java
@@ -1,51 +1,174 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-//-------------------------------------------------------------------------
 /**
- *  Test class for SortComparison.java
+ *                              Insertion       Selection      Merge        Quick
+ * numbers1000.txt              5.38ms          3.36ms         0.37ms       0.81ms
+ * numbers1000Duplicates        0.20ms          0.69ms         0.63ms       0.36ms
+ * numbersNearlyOrdered1000     0.03ms          0.36ms         0.69ms       0.07ms  
+ * numbersReverse1000           0.19ms          0.30ms         0.69ms       0.39ms
+ * numbersSorted1000            0.00ms          0.28ms         0.14ms       0.61ms    
+ * numbers10000                 8.41ms          24.90ms        2.16ms       0.93ms   
+ */
+
+/**
+ * Test class for SortComparison.java
  *
- *  @author
- *  @version HT 2020
+ * @author Sean Gaffney
+ * @version HT 2020
  */
 @RunWith(JUnit4.class)
-public class SortComparisonTest
-{
-    //~ Constructor ........................................................
+public class SortComparisonTest {
+    // ~ Constructor ........................................................
     @Test
-    public void testConstructor()
-    {
+    public void testConstructor() {
         new SortComparison();
     }
 
-    //~ Public Methods ........................................................
+    // ~ Public Methods ........................................................
 
     // ----------------------------------------------------------
     /**
      * Check that the methods work for empty arrays
      */
     @Test
-    public void testEmpty()
-    {
+    public void testEmpty() {
+        double[] a = new double[0];
+        assertEquals(a, SortComparison.selectionSort(a));
+        assertEquals(a, SortComparison.insertionSort(a));
+        assertEquals(a, SortComparison.mergeSort(a));
+        assertEquals(a, SortComparison.quickSort(a));
     }
 
+    @Test
+    public void testInsertionSort() {
+        double a[] = {6, 1, 11, 19, 4, 13, 20, 0, 10, 2, 18, 16, 3, 7, 14, 17, 8, 12, 9, 5, 15};
+        double sorted[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        assertTrue(Arrays.equals(sorted, SortComparison.insertionSort(a)));
+    }
 
-    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
-    // be executed at least once from at least one test.
+    @Test
+    public void testSelectionSort() {
+        double a[] = {6, 1, 11, 19, 4, 13, 20, 0, 10, 2, 18, 16, 3, 7, 14, 17, 8, 12, 9, 5, 15};
+        double sorted[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        assertTrue(Arrays.equals(sorted, SortComparison.selectionSort(a)));
+    }
+
+    @Test
+    public void testQuickSort() {
+        double a[] = {6, 1, 11, 19, 4, 13, 20, 0, 10, 2, 18, 16, 3, 7, 14, 17, 8, 12, 9, 5, 15};
+        double sorted[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        assertTrue(Arrays.equals(sorted, SortComparison.quickSort(a)));
+    }
+
+    @Test
+    public void testMergeSort() {
+        double a[] = {6, 1, 11, 19, 4, 13, 20, 0, 10, 2, 18, 16, 3, 7, 14, 17, 8, 12, 9, 5, 15};
+        double sorted[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        assertTrue(Arrays.equals(sorted, SortComparison.mergeSort(a)));
+    }
 
     // ----------------------------------------------------------
     /**
-     *  Main Method.
-     *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
+     * Main Method. Use this main method to create the experiments needed to answer
+     * the experimental performance questions of this assignment.
+     * 
+     * @throws FileNotFoundException
      *
      */
-    public static void main(String[] args)
-    {
-        //TODO: implement this method
+    public static void main(String[] args) throws IOException {
+        String[] files = new String[]{
+        "/Users/seangaffney/Documents/Code/SecondYear/AlgorithmsAndDataStructuresII/Assignment1/numbers1000.txt",
+        "/Users/seangaffney/Documents/Code/SecondYear/AlgorithmsAndDataStructuresII/Assignment1/numbers1000Duplicates.txt",
+        "/Users/seangaffney/Documents/Code/SecondYear/AlgorithmsAndDataStructuresII/Assignment1/numbersNearlyOrdered1000.txt",
+        "/Users/seangaffney/Documents/Code/SecondYear/AlgorithmsAndDataStructuresII/Assignment1/numbersReverse1000.txt",
+        "/Users/seangaffney/Documents/Code/SecondYear/AlgorithmsAndDataStructuresII/Assignment1/numbersSorted1000.txt",
+        "/Users/seangaffney/Documents/Code/SecondYear/AlgorithmsAndDataStructuresII/Assignment1/numbers10000.txt"
+        };
+
+        for (String file : files) {
+            ArrayList<Double> doubles = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) doubles.add(Double.parseDouble(line));
+                }
+                
+                double insertionTime = 0;
+                for(int i = 0; i < 3; i++) {
+                    double[] a = resetArray(doubles);
+                    double startTime = System.nanoTime();
+                    SortComparison.insertionSort(a);
+                    double endTime = System.nanoTime();
+                    double duration = (endTime - startTime) / 1000000;
+                    insertionTime += duration;
+                }
+                System.out.println("Insertion Sort time for " + file.split("/")[file.split("/").length - 1] + ": " + insertionTime/3 + "ms");
+                
+                double selectionTime = 0;
+                for(int i = 0; i < 3; i++) {
+                    double[] a = resetArray(doubles);
+                    double startTime = System.nanoTime();
+                    SortComparison.selectionSort(a);
+                    double endTime = System.nanoTime();
+                    double duration = (endTime - startTime) / 1000000;
+                    selectionTime += duration;
+                }
+                System.out.println("Selection Sort time for " + file.split("/")[file.split("/").length - 1] + ": " + selectionTime/3 + "ms");
+
+
+                double mergeTime = 0;
+                for(int i = 0; i < 3; i++) {
+                    double[] a = resetArray(doubles);
+                    double startTime = System.nanoTime();
+                    SortComparison.mergeSort(a);
+                    double endTime = System.nanoTime();
+                    double duration = (endTime - startTime) / 1000000;
+                    mergeTime += duration;
+                }
+                System.out.println("Merge Sort time for " + file.split("/")[file.split("/").length - 1] + ": " + mergeTime/3 + "ms");
+
+                double quickTime = 0;
+                for(int i = 0; i < 3; i++) {
+                    double[] a = resetArray(doubles);
+                    double startTime = System.nanoTime();
+                    SortComparison.quickSort(a);
+                    double endTime = System.nanoTime();
+                    double duration = (endTime - startTime) / 1000000;
+                    quickTime += duration;
+                }
+                System.out.println("Quick Sort time for " + file.split("/")[file.split("/").length - 1] + ": " + quickTime/3 + "ms");
+            }
+        }
+    }
+    private static void printArray(double[] a) {
+        for (double d : a) {
+            System.out.print(d + ", ");
+        }
+        System.out.println();
     }
 
-}
 
+    private static double[] resetArray(ArrayList<Double> doubles) {
+        double[] a;
+        a = new double[doubles.size()];
+        for (int j = 0; j < a.length; j++) {
+            a[j] = doubles.get(j);
+        }
+        return a;
+    }
+}
